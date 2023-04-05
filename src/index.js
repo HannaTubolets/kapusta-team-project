e8849ef98bfa970c9bb835a75d222173a35d480d
Hanna Tubolets <anna.tubolets@gmail.com>
2023-04-05 16:13:02 +0300

    Updated basename

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
-          <BrowserRouter basename="/reactive-cabbage">
+          <BrowserRouter basename="/kapusta-team-project">
             <App />
           </BrowserRouter>
         </ThemeProvider>
